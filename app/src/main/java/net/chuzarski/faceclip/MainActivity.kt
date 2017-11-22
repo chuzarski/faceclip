315536b74eed152ab897fde649c44baddac48c1c
Cody Huzarski <chuzarski@gmail.com>
2017-11-22 16:47:39 -0500

    Added help menu. Added support for sending faces

--- app/src/main/java/net/chuzarski/faceclip/MainActivity.kt
+++ app/src/main/java/net/chuzarski/faceclip/MainActivity.kt
@@ -1,17 +1,15 @@
 package net.chuzarski.faceclip
 
 import android.app.Activity
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
+import android.app.AlertDialog
+import android.content.*
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class MainActivity : Activity(), AdapterView.OnItemClickListener {
+class MainActivity : Activity(), AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     var buttonList: ListView? = null
     var list: Array<String>? = null
@@ -29,6 +27,12 @@ class MainActivity : Activity(), AdapterView.OnItemClickListener {
 
         log.debug("activity created!")
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
     }
 
     private fun initList(): Unit {
@@ -51,21 +55,69 @@ class MainActivity : Activity(), AdapterView.OnItemClickListener {
             }
         }
 
-        buttonList!!.setOnItemClickListener(this)
+        buttonList!!.onItemClickListener = this
+        buttonList!!.onItemLongClickListener = this
+
+    }
+
+    private fun getCurrFace(parent: AdapterView<*>?, position: Int): String {
+        if (parent != null) {
+            return parent.getItemAtPosition(position) as String
+        } else {
+            return ""
+        }
+    }
+
+    private fun handleHelpMenu(): Unit {;
+        var alertbuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+        alertbuilder
+                .setTitle(resources.getString(R.string.help_dialog_title))
+                .setMessage(resources.getString(R.string.help_dialog_content))
+                .setNeutralButton(R.string.gotcha, { dialog: DialogInterface, i: Int ->
+                    dialog.dismiss()
+                })
+        alertbuilder.create().show()
     }
 
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        var currFace = ""
+        var currFace: String = ""
         var clip: ClipData
 
-        currFace = parent!!.getItemAtPosition(position) as String
+        currFace = getCurrFace(parent, position)
 
         // need a clip
         clip = ClipData.newPlainText(currFace, currFace)
         clipboard!!.primaryClip = clip
 
         Toast.makeText(this, "\" $currFace \" copied to clipboard!", Toast.LENGTH_SHORT).show()
+    }
 
+    override fun onItemLongClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long): Boolean {
+        var currFace: String = getCurrFace(parent, position)
+        var textIntent: Intent = Intent()
+
+        if(currFace.isEmpty()) {
+            return false
+        }
+
+        textIntent.setAction(Intent.ACTION_SEND)
+        textIntent.putExtra(Intent.EXTRA_TEXT, currFace)
+        textIntent.setType("text/plain")
+        startActivity(Intent.createChooser(textIntent, currFace))
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item != null) {
+            when(item.itemId) {
+                R.id.help_menu_action -> { handleHelpMenu(); return true }
+                else -> return false
+            }
+        } else {
+            return false
+        }
     }
 
 }
